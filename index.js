0c1ddb18b214e7620bcec2791de05aee6c24d72f
CullerWhale <jcullenharris@gmail.com>
2022-05-01 23:06:25 -0500

    whoops

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const questions = [
 
   {
     type: "list",
-    name: 'initialQuestion', //employeeType
+    name: 'initialQuestion', 
     message: "What would you like to do?",
     choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
   },
